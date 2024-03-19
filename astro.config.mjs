845572f102b4b9c3de92c8fdbb2497fde8052cb9
HectorCavilla <hector.cavilla@gmail.com>
2024-03-18 19:55:28 -0500

    Add: New language - English

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,28 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import node from "@astrojs/node";
+//import vercel from "@astrojs/vercel/serverless";
 
-import vercel from "@astrojs/vercel/serverless";
+// https://astro.build/config
+/* export default defineConfig({
+  output: 'hybrid',
+  integrations: [tailwind(), react()],
+  adapter: vercel() */
+
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
   integrations: [tailwind(), react()],
-  adapter: vercel()
+  i18n: {
+    defaultLocale: "es",
+    locales: ["es", "en"],
+    routing: {
+      prefixDefaultLocale: false
+    }
+  },
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
